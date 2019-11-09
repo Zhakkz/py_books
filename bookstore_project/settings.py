5c0aee0201fd8cc3519f0a010c94b953c66bf5ae
Aday Zhumabek <aday.05.12.96@gmail.com>
2019-11-09 17:26:42 +0600

    ch4

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #local
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
+    'pages.apps.PagesConfig'
 ]
 
 
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'bookstore_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
